c312a488af63045ead5e71ffa192c52dd9658ec4
WStrellis <wstrellis@gmail.com>
2019-08-08 10:36:28 -0400

    moved api request to its own method

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -23,37 +23,44 @@ class App extends React.Component {
       })
     }
 
+    fetchAPODdata(){
+      // convert selectedDate to a format which can be used by the APOD API
+      const useDate = this.state.selectedDate.toJSON().slice(0, 10);
+
+      // create a token which can be used to end api call
+      const CancelToken= axios.CancelToken;
+      const source = CancelToken.source(); 
+
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${useDate}`,
+      { cancelToken : source.token }
+      )
+      // if the the server responds show the results
+      .then( res =>{
+        this.setState({loading : false, pod: res.data})
+        // stop polling server after data has been received
+        source.cancel();
+      })
+      // if there is an error show the error message
+      .catch( err =>{
+        this.setState({loading : false, err})
+      })
+    }// end fetchAPODdata
+
   componentDidMount() {
-    console.log(this.state.selectedDate);
-    // convert selectedDate to a format which can be used by the APOD API
-    const useDate = this.state.selectedDate.toJSON().slice(0, 10);
-    console.log(useDate);
+    this.fetchAPODdata();
+      } // end componentDidMount
 
-    // create a token which can be used to end api call
-    const CancelToken= axios.CancelToken;
-    const source = CancelToken.source(); 
+  UNSAFE_componentWillUpdate(){
+    console.log( "state  updated ",this.state.selectedDate)
+  } // end componentWillUpdate
 
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${useDate}`,
-    { cancelToken : source.token }
-    )
-    // if the the server responds show the results
-    .then( res =>{
-      this.setState({loading : false, pod: res.data})
-      // stop polling server after data has been received
-      source.cancel();
-    })
-    // if there is an error show the error message
-    .catch( err =>{
-      this.setState({loading : false, err})
-    })
-  } // end componentDidMount
-
-      // <Potd {...this.state} />
   render () {
     return (
       <Potd pod={this.state.pod} loading={this.state.loading} cb={this.changeDate} date={this.state.selectedDate}/>
     )
-  }
+  } // end  render
+
+
 } // end App
 
 export default App;
