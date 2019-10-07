62019dcd94767891f8dadc486ac2f0793d450064
WStrellis <wstrellis@gmail.com>
2019-10-06 22:25:06 -0400

    added the file thought I didn't need

---
+++ src/styles/style_variables.js
@@ -0,0 +1,12 @@
+// used by icon components
+const colors = {
+  primaryBlue: 'rgba( 3, 227, 235, 1)',
+  primaryBlueTransparent: 'rgba( 3, 227, 235, 0.16)',
+  primaryPurple: 'rgba( 83, 7, 134, 1)',
+  primaryPurpleTransparent: 'rgba( 83, 7, 134, 0.16)',
+  disabledLight: '#808080',
+  disabledDark: '#101010',
+  disabledDarkTransparent: '#101010af'
+}
+
+export { colors }
