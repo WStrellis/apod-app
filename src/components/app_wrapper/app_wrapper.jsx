615d0adeadbb6435e22904aeef4e79c440058aaf
WStrellis <wstrellis@gmail.com>
2019-10-25 20:22:24 -0400

    show loading spinner when the server is down

--- src/components/app_wrapper/app_wrapper.jsx
+++ src/components/app_wrapper/app_wrapper.jsx
@@ -11,7 +11,7 @@ const AppWrap = Styled.div`
   min-height: 100vh; 
   width: 100%;
   color: #fff;
-  background-color: #0c001e;
+  /* background-color: #0c001e; */
   /* background: -moz-linear-gradient(top, rgba(0,0,0,1) 0%, rgba(32,24,54,1) 70%, rgba(43,32,72,1) 90%, rgba(43,32,72,1) 100%);
   background: -webkit-gradient(left top, left bottom, color-stop(0%, rgba(0,0,0,1)), color-stop(70%, rgba(32,24,54,1)), color-stop(90%, rgba(43,32,72,1)), color-stop(100%, rgba(43,32,72,1)));
   background: -webkit-linear-gradient(top, rgba(0,0,0,1) 0%, rgba(32,24,54,1) 70%, rgba(43,32,72,1) 90%, rgba(43,32,72,1) 100%);
